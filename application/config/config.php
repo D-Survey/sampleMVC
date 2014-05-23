fc7419dbd158d94000b7765e80f51594a604b86a
Kanagarajah Thanushan <thanushan@info-share.org>
2014-05-23 15:20:22 +0530

    Uplode the database backup

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,7 @@
 <?php 
 
+//$config['base_url'] = 'http://tempapp.com/'; // Base URL including trailing slash (e.g. http://localhost/)
 $config['base_url'] = 'http://samplemvc/'; // Base URL including trailing slash (e.g. http://localhost/)
-
 $config['default_controller'] = 'main'; // Default controller to load
 $config['error_controller'] = 'error'; // Controller used for errors (e.g. 404, 500 etc)
 
@@ -9,5 +9,4 @@ $config['db_host'] = 'localhost'; // Database host (e.g. localhost)
 $config['db_name'] = 'survey_managament_system'; // Database name
 $config['db_username'] = 'root'; // Database username
 $config['db_password'] = ''; // Database password
-
 ?>
--- application/views/footer.php
+++ application/views/footer.php
@@ -1,12 +1,9 @@
 <hr>
-	<footer class="navbar-inverse">
-		<div class="well well-inverse btn-xs">&copy; D-Survey.com 2014</div>
+	<footer>
+		<div class="well well-sm">&copy; D-Survey.com 2014</div>
     </footer>
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="<?php echo BASE_URL; ?>static/assets/jquery/dist/jquery.min.js"></script>
-    <script src="<?php echo BASE_URL; ?>static/assets/js/bootstrap.min.js"></script>
+    <script src="/static/assets/jquery/dist/jquery.min.js"></script>
+    <script src="/static/assets/js/bootstrap.min.js"></script>
   </body>
 </html>
